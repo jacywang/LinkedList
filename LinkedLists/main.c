d97d951ba500aba2d32d34220a2d4d02340170d3
Jacy Wang <jacywj@gmail.com>
2015-05-06 19:04:28 -0700

    Task 6 - Initialization Function

--- LinkedLists/main.c
+++ LinkedLists/main.c
@@ -7,6 +7,7 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
 struct Node {
     int value;
@@ -19,52 +20,39 @@ void listAll(Node *node);
 Node *searchValue(Node *node, int val);
 void insertToTheEnd(Node *node, Node *newNode);
 void removeListItem(Node *node, Node *nodeToDelete);
+Node *initListItem(int val);
 
 int main(int argc, const char * argv[]) {
-    
-    Node node1;
-    Node node2;
-    Node node3;
-    Node node4;
-    Node node5;
-    Node newNode;
-    
-    node1.value = 1;
-    node1.next = &node2;
-    
-    node2.value = 2;
-    node2.next = &node3;
-    
-    node3.value = 3;
-    node3.next = &node4;
-    
-    node4.value = 4;
-    node4.next = &node5;
-    
-    node5.value = 5;
-    node5.next = NULL;
-    
-    newNode.value = 6;
+    Node *node1 = initListItem(1);
+    Node *node2 = initListItem(2);
+    Node *node3 = initListItem(3);
+    Node *node4 = initListItem(4);
+    Node *node5 = initListItem(5);
+    Node *newNode = initListItem(6);
+    insertToTheEnd(node1, node2);
+    insertToTheEnd(node1, node3);
+    insertToTheEnd(node1, node4);
+    insertToTheEnd(node1, node5);
 
     printf("All values in the list: ");
-    listAll(&node1);
+    listAll(node1);
     
     int valueUserInput;
     printf("Input the value to find:");
     scanf("%d", &valueUserInput);
     
-    Node *searchResult = searchValue(&node1, valueUserInput);
+    Node *searchResult = searchValue(node1, valueUserInput);
     if (searchResult == NULL) {
         printf("%d is not found in this list!\n", valueUserInput);
     } else {
         printf("Search result %d found at %p\n", searchResult->value, searchResult);
     }
     
-    insertToTheEnd(&node1, &newNode);
-    listAll(&node1);
+    insertToTheEnd(node1, newNode);
+    listAll(node1);
     
-    removeListItem(&node1, &newNode);
-    listAll(&node1);
+    removeListItem(node1, newNode);
+    listAll(node1);
     
     return 0;
 }
@@ -103,11 +91,17 @@ void removeListItem(Node *node, Node *nodeToDelete) {
             node = node->next;
         }
         if (nodeToDelete->next != NULL) {
-            node->next = nodeToDelete->next;
+            node->next = node->next->next;//nodeToDelete->next;
             nodeToDelete->next = NULL;
         } else {
             node->next = NULL;
         }
     }
+}
+
+Node *initListItem(int val) {
+    Node *node = (Node *) malloc(sizeof(Node));
+    node->value = val;
+    return node;
 }
 
